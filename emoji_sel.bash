96759807786d13fd6ddaa7100b841b8d649a9dfb
dvquentincarrel <dvquentincarrel@gmail.com>
2024-09-26 12:49:05 +0200

    [A] emoji_sel: Fuzzy emoji selection tool

---
+++ emoji_sel.bash
@@ -0,0 +1,21 @@
+#!/bin/env bash
+cache_dir=${XDG_CACHE_DIR:-~/.cache}
+if ! [[ -f $cache_dir/emojis.txt ]]; then
+    echo "Emoji file not found. Downloading it..." 1>&2
+    curl --silent https://www.unicode.org/Public/emoji/latest/emoji-test.txt > "$cache_dir"/emojis.txt
+    echo "Emojis downloaded" 1>&2
+fi
+
+read -rd '' ed_script <<EOF
+g/\(^#\|^$\)/d
+g/[A-F0-9]\+ [A-F0-9]\+ /d
+g/1F3F./d
+g/unqualified/d
+%s/^.*#//
+%s/E[0-9.]*//
+g/./p
+EOF
+
+ed "$cache_dir"/emojis.txt <<< "$ed_script" |
+fzf |
+awk '{print $1 } END {print "\n"}' ORS=''
