46a1cffcf8a94390abde2a1b31c290fc167532e2
dvquentincarrel <dvquentincarrel@gmail.com>
2024-10-09 12:47:05 +0200

    [M] Use cue-pine for installation

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Setup script, handles symlink creation
-
-lbin=${1:-"$HOME/.local/bin"}
-mkdir -p "$lbin"
-files=$(find $PWD -type d -name '.git' -prune -o -type f -not -name 'setup.sh' -perm -100 -printf "%P\n")
-for executable in $files; do
-    ln -sv "$PWD/$executable" "$lbin/$executable"
-done
