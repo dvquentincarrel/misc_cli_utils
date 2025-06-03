c6f3375acb96f36954aabb5507ae1b663a54590c
Quentin Carrel <dvquentincarrel@gmail.com>
2025-06-03 12:44:49 +0200

    [MA] doat: checks executability of arg beforehand, hides input

--- doat.bash
+++ doat.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [[ $1 == '-h' ]] || [[ $# -lt 2 ]]; then
     echo 'usage: doat <TIME DESCRIPTION> COMMAND...'
     echo 'For time description, run:'
@@ -6,13 +7,22 @@ if [[ $1 == '-h' ]] || [[ $# -lt 2 ]]; then
 fi
 epoch=$(date -d "$1" +%s)
 current=$(date +%s)
+
+if ! command -v "$2" &>/dev/null; then
+    echo -e "\x1b[31mError\x1b[m: \x1b[1m$2\x1b[m does not look like an executable or function"
+    exit 1
+fi
+
+trap "stty echo; tput cnorm; echo ' (ABORTED)'; exit" INT
+stty -echo; tput civis
+
 while [[ $current < $epoch ]]; do
     printf "\x1b[G$(date -d @$((epoch - current - 3600)) +%T)"
     sleep 1
     current=$(date +%s)
 done
-
 printf "\x1b[G$(date -d @$((epoch - current - 3600)) +%T)\n"
+stty echo; tput cnorm
 
 shift
 "$@"
